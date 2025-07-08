8faea05d41d422d6b1f374dc0dcdb34a13a1e771
Haoqing <w1838861905@gmail.com>
2025-07-09 03:21:16 +0800

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    minify: 'terser',
+    assetsDir: 'assets',
     sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
